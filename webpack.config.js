73f22b80a447421ef1cefcb1418d2643093a9c18
Mutsu0121 <mutu.0121@gmmail.com>
2023-04-19 15:36:34 +0900

    暫定

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+module.exports = {
+  entry: {
+    bundle: './src/index.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'), 
+    filename: '[name].js'  
+  },
+  resolve: {
+    extensions:['.ts','.js'] 
+  },
+  devServer: {
+    //contentBase: path.join(__dirname, 'dist'), 
+    static: {
+      directory: './dist'
+    },
+    open: true 
+  },
+
+  module: {
+    rules: [
+      {
+        
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  }
+}
