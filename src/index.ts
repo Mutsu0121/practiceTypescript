64d7ca59ca322f61a42c4aa4a428e667c2cc75fd
Mutsu0121 <mutu.0121@gmmail.com>
2023-04-25 10:29:38 +0900

    関数のパラメーターと戻り値に型をつける

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-import World from './world'
+// import World from './world'
 
-const root = document.getElementById('root')
-const world = new World('Hello world!')
-world.sayHello(root)
+// const root = document.getElementById('root')
+// const world = new World('Hello world!')
+// world.sayHello(root)
+
+// 03.基本の型定義
+// import {anySample,notExistSample, primitiveSample,unknownSample } from "./basic"
+
+// anySample()
+// notExistSample()
+// primitiveSample()
+// unknownSample()
+
+//04.関数の型定義
+import { logMessage, logMessage2 } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello Typescript')
+isUserSignedIn('ABC', 'Mutsuo')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Function parameters sample 5: ', sum)
