5df6112c3d67718cae098f90fe47be8dc48f807b
Mutsu0121 <mutu.0121@gmmail.com>
2023-04-25 11:45:22 +0900

    型エイリアス(type)でオブジェクトの型定義

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,18 @@
 // unknownSample()
 
 //04.関数の型定義
-import { logMessage, logMessage2 } from './function/basic'
-import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+// import{logMessage, logMessage2} from './function/basic'
+// import{isUserSignedIn, isUserSignedIn2, sumProductsPrice} from './function/parameters'
 
-logMessage('Hello Typescript')
-isUserSignedIn('ABC', 'Mutsuo')
-isUserSignedIn('DEF')
-isUserSignedIn2('ABC')
-const sum = sumProductsPrice(100, 200, 300, 400, 500)
-console.log('Function parameters sample 5: ', sum)
+// logMessage("Hello Typescript")
+// isUserSignedIn("ABC", "Mutsuo")
+// isUserSignedIn("DEF")
+// isUserSignedIn2("ABC")
+// const sum = sumProductsPrice(100,200,300,400,500)
+// console.log('Function parameters sample 5: 'sum)
+
+//05.オブジェクトの型定義
+import objectSample from './object/object'
+import typeAliasSample from './object/alias'
+objectSample()
+typeAliasSample()
---
+++ src/object/object.ts
@@ -0,0 +1,53 @@
+export default function objectSample() {
+  const a: object = {
+    name: 'Mutsuo',
+    age: 22,
+  }
+  //オブジェクトリテラル記法で型定義
+  let country: {
+    language: string
+    name: string
+  } = {
+    language: 'Japanese',
+    name: 'Japan',
+  }
+
+  console.log('Object object sample 1:', country)
+
+  country = {
+    language: 'English',
+    name: 'United States of America',
+  }
+  console.log('Object object sample 2:', country)
+
+  //オプショナルとreadonly
+  const torahack: {
+    age: number
+    lastName: string
+    readonly firstname: string
+    gender?: string
+  } = {
+    age: 28,
+    lastName: 'Yamada',
+    firstname: 'Tarou',
+  }
+
+  torahack.gender = 'male'
+  torahack.lastName = 'Kamado'
+  //torahack.firstname = 'Tanjiro'
+
+  console.log('Object object sample 3:', torahack)
+
+  //インデックスシグネチャ
+  const capitals: {
+    [countryName: string]: string
+  } = {
+    Japan: 'Tokyo',
+    Korea: 'Seoul',
+  }
+
+  capitals.China = 'Beijing'
+  capitals.Canada = 'Ottawa'
+
+  console.log('Object object sample 4:', capitals)
+}
