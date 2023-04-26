6042d933f30ae6d4d3c07a6a505e62163c995753
Mutsu0121 <mutu.0121@gmmail.com>
2023-04-26 09:31:29 +0900

    オブジェクト指向

--- src/oop/shogi.ts
+++ src/oop/shogi.ts
@@ -4,102 +4,100 @@ type Player = 'first' | 'second'
 
 //駒の位置を表すクラス
 class Position {
-    constructor(private suji: Suji, private dan: Dan) {}
+  constructor(private suji: Suji, private dan: Dan) {}
 
-    //パラメーターに渡された位置と現在の位置を比較するメソッド
-    distanceFrom(position: Position, player: Player) {
-        if (player === 'first') {
-            return {
-                suji: Math.abs(position.suji - this.suji),
-                dan: Math.abs(Number(position.dan) - Number(this.dan))
-            }
-        } else {
-            return {
-                suji: Math.abs(position.suji - this.suji),
-                dan: -(Math.abs(Number(position.dan) - Number(this.dan))) //段(縦の位置)は正負反転
-            }
-        }
+  //パラメーターに渡された位置と現在の位置を比較するメソッド
+  distanceFrom(position: Position, player: Player) {
+    if (player === 'first') {
+      return {
+        suji: Math.abs(position.suji - this.suji),
+        dan: Math.abs(Number(position.dan) - Number(this.dan)),
+      }
+    } else {
+      return {
+        suji: Math.abs(position.suji - this.suji),
+        dan: -Math.abs(Number(position.dan) - Number(this.dan)), //段(縦の位置)は正負反転
+      }
     }
+  }
 }
 
 //駒を表すクラス
 abstract class Piece {
-    //Pieceクラスとサブクラスでアクセスできる
-    protected position: Position
+  //Pieceクラスとサブクラスでアクセスできる
+  protected position: Position
 
-    constructor(
-        private readonly player: Player,
-        suji: Suji,
-        dan: Dan
-    ) {
-        this.position = new Position(suji, dan)
-    }
+  constructor(private readonly player: Player, suji: Suji, dan: Dan) {
+    this.position = new Position(suji, dan)
+  }
 
-    //メソッドの定義
-    //駒の移動用のメソッド
-    moveTo(position: Position) {
-        this.position = position
-    }
+  //メソッドの定義
+  //駒の移動用のメソッド
+  moveTo(position: Position) {
+    this.position = position
+  }
 
-    //移動できるかどうかを判断するメソッド
-    abstract canMoveTo(position: Position, player: Player) : boolean
+  //移動できるかどうかを判断するメソッド
+  abstract canMoveTo(position: Position, player: Player): boolean
 }
 
 class Osho extends Piece {
-    canMoveTo(position: Position, player: Player): boolean {
-        const distance = this.position.distanceFrom(position, player)
-        return (distance.suji < 2 && distance.dan < 2)
-    }
+  canMoveTo(position: Position, player: Player): boolean {
+    const distance = this.position.distanceFrom(position, player)
+    return distance.suji < 2 && distance.dan < 2
+  }
 }
 
 class Fu extends Piece {
-    canMoveTo(position: Position, player: Player): boolean {
-        const distance = this.position.distanceFrom(position, player)
-        //移動先との距離が前方1マスであれば
-        return distance.suji === 0 && distance.dan === 1
-    }
+  canMoveTo(position: Position, player: Player): boolean {
+    const distance = this.position.distanceFrom(position, player)
+    //移動先との距離が前方1マスであれば
+    return distance.suji === 0 && distance.dan === 1
+  }
 }
 
 class Narikin extends Fu {
-    canMoveTo(position: Position, player: Player): boolean {
-        const distance = this.position.distanceFrom(position, player)
-        return (
-            //移動先が1マス以内
-            distance.suji < 2 && distance.dan < 2
-            //左後方と右後方には進めない
-            && (distance.suji !== 0 && distance.dan === -1)
-        )
-    }
+  canMoveTo(position: Position, player: Player): boolean {
+    const distance = this.position.distanceFrom(position, player)
+    return (
+      //移動先が1マス以内
+      distance.suji < 2 &&
+      distance.dan < 2 &&
+      //左後方と右後方には進めない
+      distance.suji !== 0 &&
+      distance.dan === -1
+    )
+  }
 }
 
 class Game {
-    private pieces = Game.makePieces()
-    private static makePieces() {
-        return [
-            new Osho('first', 5, '1'),
-            new Osho('second', 5, '9')
+  private pieces = Game.makePieces()
+  private static makePieces() {
+    return [
+      new Osho('first', 5, '1'),
+      new Osho('second', 5, '9'),
 
-            // 先手の歩
-            new Fu('first', 1, '3'),
-            new Fu('first', 2, '3'),
-            new Fu('first', 3, '3'),
-            new Fu('first', 4, '3'),
-            new Fu('first', 5, '3'),
-            new Fu('first', 6, '3'),
-            new Fu('first', 7, '3'),
-            new Fu('first', 8, '3'),
-            new Fu('first', 9, '3'),
+      // 先手の歩
+      new Fu('first', 1, '3'),
+      new Fu('first', 2, '3'),
+      new Fu('first', 3, '3'),
+      new Fu('first', 4, '3'),
+      new Fu('first', 5, '3'),
+      new Fu('first', 6, '3'),
+      new Fu('first', 7, '3'),
+      new Fu('first', 8, '3'),
+      new Fu('first', 9, '3'),
 
-            // 後手の歩
-            new Fu('second', 1, '7'),
-            new Fu('second', 2, '7'),
-            new Fu('second', 3, '7'),
-            new Fu('second', 4, '7'),
-            new Fu('second', 5, '7'),
-            new Fu('second', 6, '7'),
-            new Fu('second', 7, '7'),
-            new Fu('second', 8, '7'),
-            new Fu('second', 9, '7'),
-        ]
-    }
+      // 後手の歩
+      new Fu('second', 1, '7'),
+      new Fu('second', 2, '7'),
+      new Fu('second', 3, '7'),
+      new Fu('second', 4, '7'),
+      new Fu('second', 5, '7'),
+      new Fu('second', 6, '7'),
+      new Fu('second', 7, '7'),
+      new Fu('second', 8, '7'),
+      new Fu('second', 9, '7'),
+    ]
+  }
 }
